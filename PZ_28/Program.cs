1c221dc4119488f5646efc06d0199c6f9fdaec03
Золин Никита <zolinnikita69@gmail.com>
2023-04-28 22:48:58 +0500

    Добавьте файлы проекта.

---
+++ PZ_28/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Counter
+    {
+        public event EventHandler<int> NumberGenerated;
+
+        public void GenerateNumbers()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                NumberGenerated?.Invoke(this, i);
+            }
+        }
+    }
+
+    class Wait
+    {
+        private readonly int _targetValue;
+
+        public Wait(int targetValue)
+        {
+            _targetValue = targetValue;
+        }
+
+        public void WaitForValue(object sender, int value)
+        {
+            if (value == _targetValue)
+            {
+                Console.WriteLine($"Target value {_targetValue} found.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            Wait wait200 = new Wait(200);
+            Wait wait800 = new Wait(800);
+
+            counter.NumberGenerated += wait200.WaitForValue;
+            counter.NumberGenerated += wait800.WaitForValue;
+
+            Thread counterThread = new Thread(counter.GenerateNumbers);
+            counterThread.Start();
+
+            Console.WriteLine("Waiting for target values...");
+
+            counterThread.Join();
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
